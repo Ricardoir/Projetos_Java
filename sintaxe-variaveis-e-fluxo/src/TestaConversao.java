40f81938a4b8efc5d333e69671b5147970e14979
BRQ\ricardoiannuzzi <ricardo.iannuzzi@brq.com>
2018-06-19 10:44:10 -0300

    first commit

---
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,26 @@
+
+public class TestaConversao {
+	
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		long numeroGrande = 324324324324L;
+		short valorPequeno = 2131;
+				
+		System.out.println(numeroGrande);
+		System.out.println(valorPequeno);
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		
+		System.out.println(total);
+		
+		
+		
+						
+	}
+
+}
